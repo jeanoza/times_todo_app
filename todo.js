cd58920cb76a01fa658934d1f1fb44b1d837e316
jeanoza <choikb916@gmail.com>
2020-12-21 11:36:51 +0100

    update greetings & background

--- todo.js
+++ todo.js
@@ -4,7 +4,20 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+function deleteTodo(event) {
+    console.log(event.target.parentNode);//parentElement too...
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li); //{parent_tag}.removeChild({child_tag})
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -18,9 +31,10 @@ function paintToDo(text) {
     const span = document.createElement("span");
     const newId =  toDos.length + 1
     delBtn.innerText = "❌";
-    span.innerText = text;
-    li.appendChild(delBtn);
+    delBtn.addEventListener("click", deleteTodo);
+    span.innerText =  `${text} `;
     li.appendChild(span);
+    li.appendChild(delBtn);
     li.id = newId; //attribute id in li tag
     toDoList.appendChild(li);
     const toDoObj = {
@@ -47,10 +61,7 @@ function loadToDos() {
         parsedToDos.forEach(function (toDo) {
             paintToDo(toDo.text);
         });
-
-
     }
-
 }
 function init() {
     loadToDos();
