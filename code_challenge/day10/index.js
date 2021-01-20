ba3efc17bab23f677d5e067b35e96f5c9454a518
jeanoza <choikb916@gmail.com>
2021-01-20 10:39:33 +0100

    add .gitignore

---
+++ code_challenge/day10/index.js
@@ -0,0 +1,11 @@
+const main = document.querySelector(".js-main"),
+    header = main.querySelector(".js-main__header"),
+    guess = main.querySelector(".js-main__guess"),
+    choice = main.querySelector(".js-main__choice"),
+    result = main.querySelector(".js-main__result");
+
+
+function init() {
+
+}
+init();
